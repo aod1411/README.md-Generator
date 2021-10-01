f65b9c5512097f1a5932ef04871fbe237750cb1a
aod1411 <nothingbutgood@comcast.net>
2021-10-01 11:13:20 -0400

    created and repo and pushed completed project

---
+++ README.md
@@ -0,0 +1,39 @@
+# ReadMe Generator
+
+  ## Description
+
+  This is a command-line application to create consistent and professional readme files.
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install the necessary depencies, run the following command:
+  npm i
+
+  ## Usage
+This is an open-source project so feel free to use.
+
+  ## Contributing
+  Feel free to submit pull requests if you would like to suggest changes!
+
+  ## Tests
+
+  To run tests, run the following command:
+  npm test
+
+  ## Questions
+
+  If you have any questions about the repository, open an issue or contact me directly at nothingbutgoodthoughts@comcast.net
+  You can find more of my work at [aod1411](https://github.com/aod1411)
+
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,57 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install the necessary depencies, run the following command:
+  ${data.installation}
+
+  ## Usage
+${data.usage}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+
+  To run tests, run the following command:
+  ${data.test}
+
+  ## Questions
+
+  If you have any questions about the repository, open an issue or contact me directly at ${data.email}
+  You can find more of my work at [${data.github}](https://github.com/${data.github})
+
+`;
+}
+
+module.exports = generateMarkdown;
